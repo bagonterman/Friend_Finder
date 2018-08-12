bfa871940b416cb02c38b743269e675d344bf301
Brett Gonterman <brettgonterman@sbcglobal.net>
2018-08-12 17:13:10 -0500

    Friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 friends = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Dummy",
+        "photo": "https://www.throwbacks.com/content/images/2017/12/CPNlCyhU8AA4k6A_GH_content_550px.jpg",
         "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1, 3, 5, 2]
     },
     {
         "name": "Brett",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "http://www.historyguide.org/images/marx-bio.jpg",
         "scores": [1, 3, 2, 1, 5, 3, 2, 5, 4, 1, 3, 1, 2]
     },
     {
